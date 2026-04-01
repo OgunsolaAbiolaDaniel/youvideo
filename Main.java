def05ed2050f4979908223bb68de1c4ebd84e67f
Abiola Daniel Ogunsola <128728750+OgunsolaAbiolaDaniel@users.noreply.github.com>
2026-04-01 10:51:34 +0100

    firstcommit

---
+++ Main.java
@@ -0,0 +1,127 @@
+import YouVideo.*;
+import java.util.Scanner;
+
+public class Main {
+    //commands
+    public static final String EXIT = "exit";
+    public static final String HELP = "help";
+    public static final String CREATE_PUBLISHABLE = "createpublishable";
+    public static final String CREATE_PREMIUM = "createpremium";
+    public static final String ADD_SUBTITLE = "addsubtitle";
+    public static final String GET_VIDEO = "getvideo";
+    public static final String SUBTITLES = "subtitles";
+    public static final String CREATE_PODCAST = "createpodcast";
+    public static final String ADD_EPISODE = "addepisode";
+    public static final String GET_PODCAST = "getpodcast";
+    public static final String EPISODES = "episodes";
+    public static final String AUTHOR_PODCASTS = "authorpodcasts";
+    public static final String REMOVE_PODCAST = "removepodcast";
+    public static final String CREATE_SHOW = "createshow";
+    public static final String GET_SHOW = "getshow";
+    public static final String REMOVE_SHOW = "removeshow";
+    public static final String REMOVE_VIDEO = "removevideo";
+
+    //messages
+    //unknown msg
+    public static final String UNKNOWN_COMMAND_MSG = "Unknown command.Type help to see available commands.";
+
+    //exit msg
+    public static final String BYE_MSG = "Bye!";
+
+    //help msg
+    public static final String HELP_MSG = """
+        createpublishable - creates a new publishable video
+        createpremium - creates a new publishable Premium video
+        addsubtitle - adds subtitle to Premium video
+        getvideo - presents publishable video data from its id
+        subtitles - Lists Premium video subtitles
+        createpodcast - creates a new podcast with no episodes
+        addepisode - adds an episode to a podcast
+        getpodcast - presents podcast data from its title
+        episodes - List podcast episodes
+        authorpodcasts - List all podcasts of an author
+        removepodcast - removes a podcast
+        createshow - creates show using an existing publishable video
+        getshow - presents show data from its title
+        removeshow - removes a show
+        removevideo - removes a publishable video
+        help - shows the available commands
+        exit - terminates the execution of the program
+        """;
+
+    //createpublishable msg
+    public static final String VIDEO_CREATED_SUCCESSFULLY = "Video %s created successfully.";
+    public static final String INVALID_LANGUAGE_TYPE = "Invalid language type.";
+    public static final String INVALID_VALUE = "Invalid value.";
+    public static final String VIDEO_ID_EXISTS = "Video with this ID already exists.";
+
+    //createpremium msg
+    public static final String INVALID_LANGUAGE_TYPE_IN_SUBTITLE = "Invalid language type in subtitle.";
+
+    //addsubtitle msg
+    public static final String SUBTITLE_ADDED_SUCCESSFULLY = "Subtitle added successfully.";
+    public static final String VIDEO_NOT_EXIST = "Video does not exist.";
+    public static final String PREMIUM_VIDEO_REQUIRED = "This operation requires a Premium video.";
+
+    //getvideo msg
+    public static final String VIDEO_INFO = "Video %s %d Title: %s";
+    public static final String VIDEO_FILE_INFO = "File: %s Publisher: %s Language: %s";
+    public static final String PUBLISHABLE_VIDEO_NOT_EXIST = "Publishable Video %s does not exist.";
+
+    //subtitles msg
+    public static final String NO_PREMIUM_VIDEO = "No Premium Video with ID.";
+    public static final String SUBTITLES_HEADER = "Subtitles for video %s:";
+    public static final String SUBTITLE_INFO = "- %s (%s)";
+
+    //createpodcast msg
+    public static final String PODCAST_CREATED_SUCCESSFULLY = "Podcast created successfully.";
+    public static final String PODCAST_TITLE_EXISTS = "Podcast with this title already exists.";
+
+    //addepisode msg
+    public static final String EPISODE_ADDED_SUCCESSFULLY = "Episode added successfully.";
+    public static final String PODCAST_NOT_EXIST = "Podcast does not exist.";
+    public static final String EPISODE_ID_EXISTS = "Episode ID already exists in the system.";
+    public static final String EPISODE_DATE_INVALID = "Episode date must be >= than latest episode date.";
+
+    //getpodcast msg
+    public static final String PODCAST_INFO = """
+                                                Podcast: %s Author: %s Language: %s
+                                                Latest episode date: %s
+                                                """;
+    //episodes msg
+    public static final String NO_EPISODES_AVAILABLE = "No episodes available for this podcast.";
+    public static final String EPISODES_HEADER = "Episodes for podcast %s:";
+    public static final String EPISODE_INFO = """
+                                            Episode %s: %d min Date: %s";
+                                            URL: %s
+                                            """;
+
+    //authorpodcasts msg
+    public static final String AUTHOR_PODCASTS_HEADER = "Podcasts by author %s:";
+    public static final String NO_PODCASTS_FOR_AUTHOR = "No podcasts found for this author.";
+
+    //removepodcast msg
+    public static final String PODCAST_REMOVED_SUCCESSFULLY = "Podcast removed successfully.";
+
+    //createshow msg
+    public static final String SHOW_CREATED_SUCCESSFULLY = "Show created successfully.";
+    public static final String VIDEO_FOR_SHOW_NOT_EXIST = "Video for show does not exist.";
+    public static final String SHOW_TITLE_ALREADY_EXISTS = "Show with this title already exists.";
+
+    //getshow msg
+    public static final String SHOW_NOT_EXIST = "Show does not exist.";
+    public static final String SHOW_INFO = """
+                                        Show Date: %s Author: %s";
+                                        Video: %s
+                                        """;
+
+    //removeshow msg
+    public static final String SHOW_REMOVED_SUCCESSFULLY = "Show removed successfully.";
+
+    //removevideo msg
+    public static final String VIDEO_REMOVED_SUCCESSFULLY = "Video removed successfully.";
+    public static final String CANNOT_REMOVE_EPISODE_VIDEO = "Cannot remove: video is an episode of a podcast.";
+    public static final String CANNOT_REMOVE_SHOW_VIDEO = "Cannot remove: video is used in a show.";
+
+
+}
